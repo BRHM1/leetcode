6ab6fcf277e098a214d2f3c3c2c2b993d0bbe7d0
BRHM1 <brhm@std.mans.edu.eg>
2024-04-11 03:36:20 +0200

    402

--- proplems.js
+++ proplems.js
@@ -1,13 +1,32 @@
-var deckRevealedIncreasing = function(deck) {
+var deckRevealedIncreasing = function (deck) {
     const res = Array(deck.length)
     const queue = Array(deck.length)
-    deck.sort((a , b) => a - b)
-    for(let i = 0; i < queue.length; i++) queue[i] = i
-    for(let card of deck){
+    deck.sort((a, b) => a - b)
+    for (let i = 0; i < queue.length; i++) queue[i] = i
+    for (let card of deck) {
         let idx = queue.shift()
         res[idx] = card
         // skip the next index
-        if(queue.length) queue.push(queue.shift())
+        if (queue.length) queue.push(queue.shift())
     }
     return res
 };
+var removeKdigits = (num, k) => {
+    let stack = []
+    if (num.length === k) return "0"
+    for (let n of num) {
+        while (stack.at(-1) > n && k > 0) {
+            stack.pop();
+            k--
+        }
+        stack.push(n)
+        while (stack.at(-1) === "0" && stack.length === 1) stack.pop()
+    }
+    return stack.slice(0, stack.length - k).join('') || "0"
+}
+console.log(removeKdigits("10200", 1)) // 200
+console.log(removeKdigits("1432219", 3)) // 1219
+console.log(removeKdigits("9", 1)) // 0
+console.log(removeKdigits("1112", 1)) // 111
+console.log(removeKdigits("43214321", 4)) // 1321
+console.log(removeKdigits("10001", 4)) // 0
