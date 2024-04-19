00ef85f2042cfe914cb2b6afb50e50921b59458e
BRHM1 <brhm@std.mans.edu.eg>
2024-04-19 02:44:20 +0200

    number of islands 200

--- proplems.js
+++ proplems.js
@@ -183,5 +183,25 @@ var islandPerimeter = function (grid) {
     }
     return res
 };
-console.log(islandPerimeter([[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]]))
-console.log(islandPerimeter([[1 , 1 , 1]]))
+
+var numIslands = function (grid) {
+    let res = 0
+    const dfs = (r , c) => {
+        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) return 0
+        if(grid[r][c] === "0") return 0
+
+        grid[r][c] = "0"
+
+        dfs(r + 1 , c)
+        dfs(r - 1 , c)
+        dfs(r , c + 1)
+        dfs(r , c - 1)
+        return 1
+    }
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            if(grid[r][c] === "1") res += dfs(r , c)
+        }
+    }
+    return res
+};
