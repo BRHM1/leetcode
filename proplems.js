d46ac2343efdaf21f026781b43236d4704d2545e
BRHM1 <brhm@std.mans.edu.eg>
2024-05-05 13:33:27 +0300

    3bkarrim twitter linkedlist question

--- proplems.js
+++ proplems.js
@@ -4,6 +4,11 @@ function TreeNode(val, left, right) {
     this.right = (right === undefined ? null : right)
 }
 
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
 var deckRevealedIncreasing = function (deck) {
     const res = Array(deck.length)
     const queue = Array(deck.length)
@@ -686,14 +691,16 @@ var findMaxK = function (nums) {
 var compareVersion = function (version1, version2) {
     let v1 = version1.split(".")
     let v2 = version2.split(".")
-    for(let i = 0; i < Math.max(v1.length , v2.length); i++){
+    for (let i = 0; i < Math.max(v1.length, v2.length); i++) {
         let num1 = i < v1.length ? parseInt(v1[i]) : 0
         let num2 = i < v2.length ? parseInt(v2[i]) : 0
-        if(num1 > num2) return 1
-        if(num1 < num2) return -1
+        if (num1 > num2) return 1
+        if (num1 < num2) return -1
     }
     return 0
 };
-console.log(compareVersion("1.0", "1.0.1")) // 0
-console.log(compareVersion("1.01", "1.001")) // 0
-console.log(compareVersion("0.1", "1.1")) //-1
+
+var deleteNode = function (node) {
+    node.val = node.next.val
+    node.next = node.next.next
+};
